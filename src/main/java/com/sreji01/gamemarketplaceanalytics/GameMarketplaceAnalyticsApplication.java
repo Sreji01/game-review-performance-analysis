7780abb2f5ada59f40e963d098941ff9f523676e
Marko Srejic <marko.srejic@deversity.net>
2026-05-27 12:49:37 +0200

    Set up Spring Boot application

---
+++ src/main/java/com/sreji01/gamemarketplaceanalytics/GameMarketplaceAnalyticsApplication.java
@@ -0,0 +1,12 @@
+package com.sreji01.gamemarketplaceanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMarketplaceAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMarketplaceAnalyticsApplication.class, args);
+    }
+}
